01817059ff2b1371d029d8a788726074355f165c
tobiaspasinato <tobiasmarpasinato2005@gmail.com>
2024-03-07 20:12:41 -0300

    0.0.00

--- random/note1.go
+++ random/note1.go
@@ -5,9 +5,18 @@ import(
 )
 
 func main(){
-	var num1,num2 int
-	fmt.Println("Cuales son tus numeros? ")
-	fmt.Scanln(&num1, &num2)
-	resultado := num1 + num2
-	fmt.Println("Tu resultado es ", resultado)
+	var edad int
+	fmt.Println("Cual es tu edad? ")
+	fmt.Scanln(&edad)
+	if(edad > 17){
+		fmt.Println("tu edad es ", edad, ", sos mayor de edad")
+	} else {
+		if(edad > 12 && edad < 18){
+			fmt.Println("tu edad es ", edad, ", sos un adolecente")
+		} else {
+			if(edad < 12){
+				fmt.Println("tu edad es ", edad, ", sos un niño")
+			}
+		}
+	}
 }
